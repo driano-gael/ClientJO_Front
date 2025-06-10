909b63d2e1c3a476db31cfad30063db6b3db6ff8
driano-gael <97949425+driano-gael@users.noreply.github.com>
2025-06-10 21:03:35 +0200

    Ajout des theme et configuraion pour daisyUI

--- application/src/app/page.tsx
+++ application/src/app/page.tsx
@@ -1,7 +1,9 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
     <>
-    <p>ici se tiendra le site client du projet studi</p>
+    <Header/>
     </>
   );
 }
--- application/src/components/Header.tsx
+++ application/src/components/Header.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="bg-gray-800 text-white p-4">
-      <nav className="container mx-auto flex justify-between">
+    <header className="bg-base-100 p-4">
+      <nav className="container bg-base-200 text-black mx-auto flex justify-between">
         <Link href="/">Accueil</Link>
         <Link href="/offre">Offres</Link>
       </nav>
