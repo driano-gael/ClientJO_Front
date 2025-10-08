f4d89e1fd2fba1868b49a4e142467cdea7916921
g.driano <driano.gael@gmail.com>
2025-10-08 14:45:14 +0200

    gestion du modal pour afficher le qrCode

--- application/src/lib/api/service/qrCodeService.ts
+++ application/src/lib/api/service/qrCodeService.ts
@@ -8,7 +8,7 @@ export class QrCodeService {
    * Récupère le qrcode d'un ticket client
    */
   static async getQrCodeClient(_ticketID: number): Promise<QRCode> {
-    const url = `${this.BASE_PATH}/create_by_ticket`;
+    const url = `${this.BASE_PATH}/create_by_ticket/`;
     return fetchApi<QRCode>(url, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
